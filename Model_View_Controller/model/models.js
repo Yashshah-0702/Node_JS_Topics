ec294d363c91dbf2b8af7c9d37ef559235576f08
yash shah <yash722002@gmail.com>
2023-06-22 14:12:22 +0530

    new_things_added

--- Model_View_Controller/model/models.js
+++ Model_View_Controller/model/models.js
@@ -1,4 +1,6 @@
-const product=[]
+const fs = require('fs')
+const path = require('path')
+
 module.exports=class Product{
 
     constructor(t){
@@ -6,10 +8,39 @@ module.exports=class Product{
     }
 
     save(){
-        product.push(this)
+        let product=[]
+        const p = path.join(path.dirname(process.mainModule.filename),'data','products.json')
+        fs.readFile(p,(err,fileContent)=>{
+            if(!err){
+                product=JSON.parse(fileContent)
+            }
+            product.push(this)
+        fs.writeFile(p,JSON.stringify(product),(err)=>{console.log(err)})
+        })
+        // product.push(this)
     }
 
     static fetchAll(){
+        const p = path.join(path.dirname(process.mainModule.filename),'data','products.json')
+        fs.readFile(p,(err,fileContent)=>{
+            if(err){
+                return []
+            }
+            return JSON.parse(fileContent)
+        })
         return product
     }
 }
+
+
+// let product = []
+        // const p = path.join(path.dirname(process.mainModule.filename),'data','product.json')
+        // fs.readFile(p,(err,fileContent)=>{
+        //     if(!err){
+        //         product = JSON.parse(fileContent)
+        //     }
+        //     product.push(this)
+        //     fs.writeFile(p,JSON.stringify(product),(err)=>{
+        //         console.log(err)
+        //     })
+        // })
